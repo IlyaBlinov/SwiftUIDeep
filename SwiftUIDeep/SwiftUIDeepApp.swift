711af10d6519bfb0550e9a3e2d26517c050939af
Ilya Blinov <ilyablinov@icloud.com>
2024-07-19 09:41:31 +0300

    Initial Commit

---
+++ SwiftUIDeep/SwiftUIDeepApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIDeepApp.swift
+//  SwiftUIDeep
+//
+//  Created by Илья Блинов on 19.07.2024.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIDeepApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
